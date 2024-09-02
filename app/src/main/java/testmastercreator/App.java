9970b9b73f074af5b50954af80816257e0763d2f
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-09-02 22:48:27 +0200

    Simple probram to chek it setup was corect, everithing works

--- app/src/main/java/testmastercreator/App.java
+++ app/src/main/java/testmastercreator/App.java
@@ -3,12 +3,26 @@
  */
 package testmastercreator;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class App extends Application 
+{
+
+    @Override
+    public void start(Stage stage) {
+        Label helloWorld = new Label("Hello, World!");
+        Scene scene = new Scene(helloWorld, 400, 200);
+        stage.setTitle("Hello JavaFX");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        launch();
+        System.out.println("Hello, World!");
     }
 }
+
