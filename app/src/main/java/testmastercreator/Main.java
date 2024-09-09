8aee2bafae6c0fbb863326b4a7d84c45ab3d1adf
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-09-09 12:22:19 +0200

    main program chages

---
+++ app/src/main/java/testmastercreator/Main.java
@@ -0,0 +1,9 @@
+package testmastercreator;
+
+
+// When give as a entery point class wich extend aplication it just inst working
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
