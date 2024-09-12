6cddd4b9067e2257e67f007dc31cefc6913f8ebe
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-09-12 22:34:39 +0200

    Start scene controler

---
+++ app/src/main/java/testmastercreator/StartSceneControler.java
@@ -0,0 +1,10 @@
+package testmastercreator;
+
+import javafx.fxml.FXML;
+
+public class StartSceneControler {
+    @FXML
+    public void startTest() throws Exception {
+        App.setRoot("QuestionScene");
+    }
+}
