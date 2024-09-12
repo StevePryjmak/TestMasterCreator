aef5df7d68c805d7ebba1f3e844b5808701e95bb
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-09-12 22:35:47 +0200

    experimentig with app

--- app/src/main/java/testmastercreator/App.java
+++ app/src/main/java/testmastercreator/App.java
@@ -2,6 +2,7 @@
 package testmastercreator;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -12,15 +13,26 @@ import javafx.stage.Stage;
 
 public class App extends Application 
 {
+    private static Stage stage;
 
     @Override
     public void start(Stage stage) throws IOException {
+        App.stage = stage;
         Parent root = FXMLLoader.load(getClass().getResource("/StartScene.fxml"));
         Scene scene = new Scene(root);
         //scene.getStylesheets().add("/styles/Styles.css");
         stage.setScene(scene);
         stage.show();
     }
+
+    public static void setRoot(String fxml) throws IOException {
+        
+        Parent root = FXMLLoader.load(App.class.getResource("/" + fxml + ".fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+    }
+
+
 
     public static void main(String[] args) {
         launch(args);
