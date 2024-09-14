9203f8a977b677c24fdd315eaed15f4d2488974f
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-09-14 20:54:34 +0200

    Base class serves only dynamic polimorfizm porpes

---
+++ app/src/main/java/testmastercreator/AbstractQuestion.java
@@ -0,0 +1,13 @@
+package testmastercreator;
+
+import javafx.scene.Scene;
+
+public abstract class AbstractQuestion {
+    protected Scene scene;
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public abstract boolean checkAnswer(String answer);
+}
