ea0ee6e6ac4663098dc25dc36ab5208273a1150e
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-11-23 19:08:49 +0100

    Tested initialisation of Test from server data

--- client/src/main/java/com/LerningBara/controller/StartSceneControler.java
+++ client/src/main/java/com/LerningBara/controller/StartSceneControler.java
@@ -12,6 +12,35 @@ public class StartSceneControler {
     
     @FXML
     public void startTest() throws Exception {
+        this.getTest();
         App.getInstance().runExampleTest();
+    }
+
+    public void getTest() {
+        System.out.println("Connected to server");
+        App.client = new Client("localhost", 8080);
+        App.client.listenForMessages();
+        App.client.sendMessage("Give me the test");
+        Object received = App.client.getOneRecivedObject();
+        if(received instanceof TestData) {
+            TestData testData = (TestData) received;
+            List<AbstractQuestionData> questions = testData.questions;
+            Test test = new Test(questions, true);
+            App.getInstance().setTest(test);
+            App.getInstance().runExampleTest();
+        }
+        // while (true) {
+        //     System.err.println("Waiting for test data...");
+        //     // Object received = client.getOneRecivedObject();
+        //     // if (received != null) {
+        //     //     if (received instanceof TestData) {
+        //     //         TestData testData = (TestData) received;
+        //     //         List<AbstractQuestionData> questions = testData.questions;
+        //     //         Test test = new Test(questions, true); // Use 'new' keyword to instantiate
+        //     //         System.out.println(test);
+        //     //         break;
+        //     //     }
+        //     // }
+        // }
     }
 }
