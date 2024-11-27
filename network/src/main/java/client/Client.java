9a9db01403fdf1db8222cd3d84ac247672bea6d2
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-11-27 16:29:23 +0100

    Refactor client code only for object comunication stream

--- network/src/main/java/client/Client.java
+++ network/src/main/java/client/Client.java
@@ -3,9 +3,11 @@ package client;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.MessageDigest;
 import java.util.Scanner;
 
 import QuestionData.TestData;
+import connection.Message;
 import QuestionData.MyClass;
 
 import java.util.List;
@@ -14,54 +16,22 @@ import java.util.ArrayList;
 public class Client {
     
     private Socket socket;
-    private BufferedReader reader;
-    private BufferedWriter writer;
-    private List<Object> recived = new ArrayList<>();
+    private List<Message> recived = new ArrayList<>();
     private ObjectInputStream objectInputStream;
+    private ObjectOutputStream objectOutputStream;
 
     public Client(String ip, int port) {
         try {
             socket = new Socket(ip, port);
-            writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
             objectInputStream = new ObjectInputStream(socket.getInputStream());
             listenForObjects();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
 
-    public void sendMessages() {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            while(socket.isConnected()) {
-                String message = scanner.nextLine();
-                writer.write(message);
-                writer.newLine();
-                writer.flush();
-            }
-            scanner.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void listenForMessages() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // try {
-                //     while (!socket.isClosed()) {
-                //         //String message = reader.readLine();
-                //         //System.out.println("Server: " + message);
-                //     }
-                // } catch (IOException e) {
-                //     e.printStackTrace();
-                // }
-            }
-        }).start();
-    }
-
+    // shorten implemetation of functional interface
     public void listenForObjects() {
         new Thread(() -> {
             try {
@@ -69,22 +39,14 @@ public class Client {
                     Object receivedObject = objectInputStream.readObject(); 
                     System.err.println("Received object: ");
 
-                    if (receivedObject instanceof TestData) {
-                        TestData testData = (TestData) receivedObject;
-                        recived.add(testData);
+                    if (receivedObject instanceof Message) {
+                        Message message = (Message) receivedObject;
+                        recived.add(message);
                         System.out.println(recived.size());
                         System.out.println("Received a TestData object");
                     } else {
                         System.out.println("Received an unknown type of object");
                     }
-                    // if (receivedObject instanceof MyClass) {
-                    //     MyClass myObject = (MyClass) receivedObject;
-                    //     System.out.println("Received object: " + myObject);
-                    //     System.out.println("Name: " + myObject.getName());
-                    //     System.out.println("Value: " + myObject.getValue());
-                    // } else {
-                    //     System.out.println("Unexpected object type received.");
-                    // }
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -92,65 +54,8 @@ public class Client {
         }).start();
     }
     
-
-    // public void listenForObjects() {
-    //     new Thread(new Runnable() {
-    //         @Override
-    //         public void run() {
-    //             try {
-
-    //                 while (!socket.isClosed()) {
-                    
-    //                     ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-                        
-    //                     Object receivedObject = ois.readObject(); // Read and deserialize the object
-    //                     System.err.println("Received object: " + receivedObject);
-    //                     // Cast the received object back to MyClass
-    //                     if (receivedObject instanceof MyClass) {
-    //                         MyClass myObject = (MyClass) receivedObject;
-    //                         System.out.println("Received object: " + myObject);
-    //                         System.out.println("Name: " + myObject.getName());
-    //                         System.out.println("Value: " + myObject.getValue());
-    //                     } else {
-    //                         System.out.println("Unexpected object type received.");
-    //                     }
-
-    //                 // ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-    //                 // Object receivedObject = objectInputStream.readObject();
-    //                 // recived.add(receivedObject);
-    //                 // if(receivedObject instanceof TestData) {
-    //                 //     TestData testData = (TestData) receivedObject;
-    //                 //     System.out.println("Received a TestData object");
-                        
-    //                 // }
-
-    //                 // if (receivedObject instanceof SingleChoiceQuestion) {
-    //                 //     SingleChoiceQuestion question = (SingleChoiceQuestion) receivedObject;
-    //                 //     System.out.println("Received a SingleChoiceQuestion");
-    //                 // } else if (receivedObject instanceof MultipleChoiceQuestion) {
-    //                 //     MultipleChoiceQuestion question = (MultipleChoiceQuestion) receivedObject;
-    //                 //     System.out.println("Received a MultipleChoiceQuestion");
-    //                 // } else {
-    //                 //     System.out.println("Received an unknown type of object");
-    //                 // }
-    //             }
-    //         } catch (Exception e) {
-    //             e.printStackTrace();
-    //         }
-    //     }}).start();
-    // }
     
-    public void sendMessage(String message) {
-        try {
-            writer.write(message);
-            writer.newLine();
-            writer.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public Object getOneRecivedObject () {
+    public Message getOneRecivedObject () {
         while(true) {
             System.out.println("Waiting for object");
             if (recived.size() > 0) {
@@ -161,6 +66,16 @@ public class Client {
         //return null;
     }
 
+    public void sendObject(Object object) {
+        try {
+            System.out.println("Sending object");
+            objectOutputStream.writeObject(object); // Serialize and send the object
+            objectOutputStream.flush();
+            System.out.println("Object sent: ");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
     public void closeConnection() {
