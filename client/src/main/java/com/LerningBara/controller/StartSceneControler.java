672155298411f667d37ec566fd51b258335029dd
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-11-28 17:48:08 +0100

    example of usage object stream

--- client/src/main/java/com/LerningBara/controller/StartSceneControler.java
+++ client/src/main/java/com/LerningBara/controller/StartSceneControler.java
@@ -7,6 +7,7 @@ import QuestionData.AbstractQuestionData;
 import javafx.fxml.FXML;
 import java.util.List;
 import client.Client;
+import connection.Message;
 
 public class StartSceneControler {
     
@@ -18,29 +19,19 @@ public class StartSceneControler {
 
     public void getTest() {
         System.out.println("Connected to server");
-        App.client = new Client("localhost", 8080);
-        App.client.listenForMessages();
-        App.client.sendMessage("Give me the test");
-        Object received = App.client.getOneRecivedObject();
-        if(received instanceof TestData) {
-            TestData testData = (TestData) received;
+        App.getInstance().client = new Client("localhost", 8080);
+        Message message = new Message("Give me the test", null);
+        App.getInstance().client.sendObject(message);
+        System.out.println("Waiting for test");
+        
+        Message messageReceived = App.getInstance().client.getOneRecivedObject();;
+        Object r = messageReceived.getObject();
+        if(r instanceof TestData) {
+            TestData testData = (TestData) r;
             List<AbstractQuestionData> questions = testData.questions;
             Test test = new Test(questions, true);
             App.getInstance().setTest(test);
             App.getInstance().runExampleTest();
         }
-        // while (true) {
-        //     System.err.println("Waiting for test data...");
-        //     // Object received = client.getOneRecivedObject();
-        //     // if (received != null) {
-        //     //     if (received instanceof TestData) {
-        //     //         TestData testData = (TestData) received;
-        //     //         List<AbstractQuestionData> questions = testData.questions;
-        //     //         Test test = new Test(questions, true); // Use 'new' keyword to instantiate
-        //     //         System.out.println(test);
-        //     //         break;
-        //     //     }
-        //     // }
-        // }
     }
 }
