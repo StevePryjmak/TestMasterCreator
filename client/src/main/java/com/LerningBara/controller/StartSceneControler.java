2186d04291655b10e9ed367f718b253593209394
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2024-11-30 19:14:17 +0100

    Add test window controler and refactor stat scene controler

--- client/src/main/java/com/LerningBara/controller/StartSceneControler.java
+++ client/src/main/java/com/LerningBara/controller/StartSceneControler.java
@@ -1,37 +1,44 @@
 package com.LerningBara.controller;
 
 import com.LerningBara.app.App;
-import QuestionData.TestData;
+import TestData.TestData;
 import com.LerningBara.model.Test;
 import QuestionData.AbstractQuestionData;
 import javafx.fxml.FXML;
+
+import java.io.IOException;
 import java.util.List;
 import client.Client;
 import connection.Message;
+
+import TestData.*;
 
 public class StartSceneControler {
     
     @FXML
     public void startTest() throws Exception {
-        this.getTest();
-        App.getInstance().runExampleTest();
+        this.getTestsList();
     }
 
-    public void getTest() {
+    public void getTestsList() {
         System.out.println("Connected to server");
         App.getInstance().client = new Client("localhost", 8080);
-        Message message = new Message("Give me the test", null);
+        Message message = new Message("List of tests", null);
         App.getInstance().client.sendObject(message);
-        System.out.println("Waiting for test");
+        System.out.println("Waiting for list of tests");
         
         Message messageReceived = App.getInstance().client.getOneRecivedObject();;
         Object r = messageReceived.getObject();
-        if(r instanceof TestData) {
-            TestData testData = (TestData) r;
-            List<AbstractQuestionData> questions = testData.questions;
-            Test test = new Test(questions, true);
-            App.getInstance().setTest(test);
-            App.getInstance().runExampleTest();
+        if(r instanceof AvalibleTestsList) {
+            AvalibleTestsList avalibleTestsList = (AvalibleTestsList) r;
+            List<TestInfoData> tests = avalibleTestsList.getTests();
+            try {
+                App.getInstance().showTestsList(tests);
+            }
+            catch (IOException e) {
+                System.out.println(e);
+            }
+            
         }
     }
 }
