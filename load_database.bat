017fef8b532871209123ef25bbabbf35454b533b
. <.>
2024-12-01 17:10:51 +0100

    .

---
+++ load_database.bat
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker exec oracle-db ./setPassword.sh projektpap2024
+
+docker cp create_database.sql oracle-db:/tmp/sql_commands.sql
+
+docker exec oracle-db sqlplus system/projektpap2024@localhost/FREEPDB1 @/tmp/sql_commands.sql
