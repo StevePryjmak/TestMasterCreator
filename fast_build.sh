9e1af7c9788fe6fdcece4139815cbbe8b04454d8
Pawel <01187285@pw.edu.pl>
2024-12-04 00:49:46 +0100

    added fast build

---
+++ fast_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Run Gradle build tasks
+echo "Running Gradle wrapper tasks..."
+if [ -f "./gradlew" ]; then
+  chmod +x ./gradlew
+  ./gradlew build || error_exit "Gradle build failed."
+else
+  error_exit "Gradle wrapper (gradlew) not found in the current directory."
+fi
+
+# Start the server and client
+
+/usr/bin/dbus-launch /usr/bin/gnome-terminal -- bash -ic "./gradlew :server:run; exec bash"
+./gradlew :client:run 
+
+echo "All tasks completed successfully!"
