5e002a76c1cb98130a8d06d29fd8c0075c1e08b9
Pawel <01187285@pw.edu.pl>
2024-12-03 23:51:03 +0100

    add delay to script

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 # oracle stuff
 docker run -d --name oracle-db -p 1521:1521 container-registry.oracle.com/database/free:23.5.0.0-lite
 
+sleep 90
+
 docker exec oracle-db ./setPassword.sh projektpap2024
 
 docker cp clean_database.sql oracle-db:/tmp/sql_commands.sql
