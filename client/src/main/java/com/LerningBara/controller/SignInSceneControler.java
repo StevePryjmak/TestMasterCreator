25a2638d6380f1c7ccfb79b819f91630f22e31e5
Hanna Zarzycka <01187305@pw.edu.pl>
2025-01-08 20:18:20 +0100

    Add database connection fix

--- client/src/main/java/com/LerningBara/controller/SignInSceneControler.java
+++ client/src/main/java/com/LerningBara/controller/SignInSceneControler.java
@@ -9,7 +9,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Label;
-import database.DataBase;
 
 public class SignInSceneControler {
    @FXML
@@ -21,9 +20,8 @@ public class SignInSceneControler {
    @FXML
    private Label loginLabel;
 
-
    @FXML
-   public void signIn() throws Exception{
+   public void signIn() throws Exception {
       String login = loginField.getText();
       String password = passwordField.getText();
       Boolean user_exists = false;
@@ -35,8 +33,8 @@ public class SignInSceneControler {
       System.out.println("Waiting for list of tests");
       Message messageReceived = App.getInstance().client.getOneRecivedObject();
       Object obj = messageReceived.getObject();
-      if (obj instanceof Boolean){
-         user_exists = (Boolean)obj;
+      if (obj instanceof Boolean) {
+         user_exists = (Boolean) obj;
       }
       App.getInstance().client = new Client("localhost", 8080);
       System.out.println("Connected to server");
@@ -44,26 +42,25 @@ public class SignInSceneControler {
       System.out.println("Waiting for list of tests");
       messageReceived = App.getInstance().client.getOneRecivedObject();
       obj = messageReceived.getObject();
-      if (obj instanceof Boolean){
-         corr_pass= (Boolean)obj;
+      if (obj instanceof Boolean) {
+         corr_pass = (Boolean) obj;
       }
 
-      if (user_exists){
+      if (user_exists) {
          loginLabel.setText("");
-         if (DataBase.checkPassword(login, password)){
+         if (corr_pass) {
             App.setRoot("StartScene");
-         }
-         else{
+         } else {
             passLabel.setText("Incorrect password.");
          }
-      }
-      else {
+      } else {
          passLabel.setText("");
          loginLabel.setText("Username not found.");
       }
    }
+
    @FXML
-   public void goBack() throws Exception{
+   public void goBack() throws Exception {
       App.setRoot("LoginScene");
    }
 }
