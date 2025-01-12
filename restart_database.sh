97e414b431639f8af735fa4d68b582a8236c6a57
Pawel <01187285@pw.edu.pl>
2025-01-12 17:22:33 +0100

    make scripts executable

--- restart_database.sh
+++ restart_database.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 # oracle stuff
-docker run -d --name oracle-db -p 1521:1521 container-registry.oracle.com/database/free:23.5.0.0-lite
-
-sleep 90
-
 docker exec oracle-db ./setPassword.sh projektpap2024
 
 docker cp clean_database.sql oracle-db:/tmp/sql_commands.sql
@@ -19,18 +15,3 @@ docker cp Insert_data.sql oracle-db:/tmp/sql_commands.sql
 
 docker exec oracle-db sqlplus system/projektpap2024@localhost/FREEPDB1 @/tmp/sql_commands.sql
 
-# Run Gradle build tasks
-echo "Running Gradle wrapper tasks..."
-if [ -f "./gradlew" ]; then
-  chmod +x ./gradlew
-  ./gradlew build || error_exit "Gradle build failed."
-else
-  error_exit "Gradle wrapper (gradlew) not found in the current directory."
-fi
-
-# Start the server and client
-
-(./gradlew :client:run ) &
-./gradlew :server:run 
-
-echo "All tasks completed successfully!"
