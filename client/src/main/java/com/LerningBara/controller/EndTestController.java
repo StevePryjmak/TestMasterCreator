f14d7c77e6a95491a00333db0bd34b1421237705
Pawel <01187285@pw.edu.pl>
2025-01-12 06:33:11 +0100

    implement end of quiz scene

--- client/src/main/java/com/LerningBara/controller/EndTestController.java
+++ client/src/main/java/com/LerningBara/controller/EndTestController.java
@@ -1,5 +1,11 @@
 package com.LerningBara.controller;
 
+import com.LerningBara.app.App;
+
+import TestData.TestData;
+import TestData.TestInfoData;
+import client.Client;
+import connection.Message;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,22 +17,44 @@ public class EndTestController {
     @FXML
     private Button likeButton;
     @FXML
-    private Label rerultLabel;
+    private Label resultLabel;
 
+    private TestInfoData currentTest;
+
+    // TODO: check if this works
     @FXML
     public void addToLiked() {
-        // add to table Likes
-        // add command to database.java
-        // add handle to client handler
+        App.getInstance().client = new Client("localhost", 8080);
+        System.out.println("Connected to server");
+        App.getInstance().client.sendMessage("Add to likes", currentTest);
+        System.out.println("Waiting to add test to likes");
+        App.getInstance().client.getOneRecivedObject();
+        App.getInstance().client.closeConnection();
     }
 
+    // TODO: check if this works
     @FXML
     public void returnToBrowsing() {
-        // return to main layout (hypa)
+        App.setRoot("MainLayoutScene");
     }
 
+    // TODO: check if this works
     @FXML
-    private void initialize() {
-        // setting rerultLabel to result
+    private void initialize(int result) {
+        // TODO: implement getting TestInfoData
+
+        App.getInstance().client = new Client("localhost", 8080);
+        System.out.println("Connected to server");
+        App.getInstance().client.sendMessage("Give me the test", currentTest.name);
+        System.out.println("Waiting for the test");
+        Message messageReceived = App.getInstance().client.getOneRecivedObject();
+        // App.getInstance().client.closeConnection();
+
+        Object currentTest = messageReceived.getObject();
+        if (currentTest instanceof TestData) {
+            TestData currentTestData = (TestData) currentTest;
+            String text = currentTestData.questions.size() + "/" + result;
+            resultLabel.setText(text);
+        }
     }
 }
