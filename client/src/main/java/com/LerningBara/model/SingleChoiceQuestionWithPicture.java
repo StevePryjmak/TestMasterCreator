797da10ef516ff3f45e194d44a26f651a292a564
Andrii-Stepan Pryimak <andrzejstepan10@gmail.com>
2025-01-15 03:45:04 +0100

    change image with picture container...

--- client/src/main/java/com/LerningBara/model/SingleChoiceQuestionWithPicture.java
+++ client/src/main/java/com/LerningBara/model/SingleChoiceQuestionWithPicture.java
@@ -16,6 +16,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.geometry.Pos;
 
 public class SingleChoiceQuestionWithPicture extends AbstractQuestion {
 
@@ -60,22 +61,63 @@ public class SingleChoiceQuestionWithPicture extends AbstractQuestion {
     public VBox getDetailsBox(int index) {
         VBox questionBox = new VBox(10);
         questionBox.getStyleClass().add("question-box");
-
+    
+        questionBox.setOnMouseClicked(event -> {
+            App.createTestController.handleQuestionEdit(index, false);
+        });
+    
         Label questionLabel = new Label("Question #" + (index + 1) + ": " + questionData.getQuestion());
         questionLabel.getStyleClass().add("question-label");
-
+        questionBox.getChildren().add(questionLabel);
+    
+        // Question image
         ImageView imageView = new ImageView(byteArrayToImage(questionData.getImage()));
         imageView.setFitWidth(200);
         imageView.setPreserveRatio(true);
-
+        questionBox.getChildren().add(imageView);
+    
+        // Answer options
+        List<String> options = questionData.getOptions();
         HBox answersBox = new HBox(10);
-        for (String option : questionData.getOptions()) {
-            RadioButton answerButton = new RadioButton(option);
+        answersBox.getStyleClass().add("answers-box");
+        answersBox.setAlignment(Pos.CENTER_LEFT);
+    
+        for (int i = 0; i < options.size(); i++) {
+            RadioButton answerButton = new RadioButton(options.get(i));
+            answerButton.setUserData(i);
             answerButton.setDisable(true);
+    
+            // Style the correct answer
+            if (i == questionData.getCorrectAnswerIndex()) {
+                answerButton.getStyleClass().add("correct-answer");
+                answerButton.setSelected(true);
+            } else {
+                answerButton.getStyleClass().add("incorrect-answer");
+            }
+    
             answersBox.getChildren().add(answerButton);
+    
+            // Add rows of answer buttons
+            if (i % 2 == 1 || i == options.size() - 1) {
+                questionBox.getChildren().add(answersBox);
+                answersBox = new HBox(10);
+                answersBox.getStyleClass().add("answers-box");
+                answersBox.setAlignment(Pos.CENTER_LEFT);
+            }
         }
-
-        questionBox.getChildren().addAll(questionLabel, imageView, answersBox);
+    
+        // Delete button
+        Image deleteImage = new Image(getClass().getResourceAsStream("/images/delete.png"));
+        ImageView deleteImageView = new ImageView(deleteImage);
+        deleteImageView.setFitHeight(20);
+        deleteImageView.setFitWidth(20);
+        deleteImageView.setOnMouseClicked(event -> {
+            App.createTestController.handleDeleteQuestion(index);
+            event.consume();
+        });
+        questionBox.getChildren().add(deleteImageView);
+    
         return questionBox;
     }
+    
 }
